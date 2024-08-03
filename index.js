b4636854965e66683b839555f4c2d3392eaeedbe
Tyler Payne <169082073+edamama@users.noreply.github.com>
2024-08-03 13:01:19 -0500

    1
    Added starter initialization code. Additional functions have not been developed.

---
+++ index.js
@@ -0,0 +1,67 @@
+const inquirer = require('inquirer');
+
+
+
+
+
+//initialize app function
+
+const questions = ["What action would you like to perform?",]
+
+function init() {
+
+    inquirer
+        .prompt({
+            
+            type: "list",
+            message: questions[0],
+            name:"menuSelection",
+            choices: ["View all departments","View all roles",
+                
+                "View all employees","Add a department","Add a role","Add an employee","Update an employee role"
+
+
+            ]
+
+        }).then(response => {
+
+            if(response.menuSelection === "View all departments"){
+
+                viewAllDepartments();
+
+            } else if(response.menuSelection === "View all roles"){
+
+                viewAllRoles();
+
+            } else if(response.menuSelection === "View all employees"){
+
+                viewAllEmployees();
+
+            } else if(response.menuSelection === "Add a department"){
+
+                addADepartment();
+
+            } else if(response.menuSelection === "Add a role"){
+
+                addARole();
+
+            } else if(response.menuSelection === "Add an employee"){
+
+                addAnEmployee();
+
+            } else if(response.menuSelection === "Update an employee role"){
+
+                updateAnEmployeeRole();
+
+            }
+
+
+        })
+            
+}
+    
+    
+
+//run program
+
+init();
